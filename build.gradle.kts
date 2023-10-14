e1c8f6fd3ae7d157975db96793a9e399a0c728be
David Jaraba <velcas669@gmail.com>
2023-10-14 18:32:14 +0200

    Init methods

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    id("java")
+    id("io.freefair.lombok") version "8.3"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.projectreactor:reactor-core:3.5.10")
+    implementation("io.r2dbc:r2dbc-h2:1.0.0.RELEASE")
+    implementation("io.r2dbc:r2dbc-pool:1.0.0.RELEASE")
+    implementation("ch.qos.logback:logback-classic:1.4.11")
+
+    testImplementation(platform("org.junit:junit-bom:5.9.2"))
+    testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ src/main/java/dev/Main.java
@@ -0,0 +1,19 @@
+package dev;
+
+import dev.services.database.DatabaseManager;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        DatabaseManager databaseManager = DatabaseManager.getInstance();
+
+
+
+
+
+
+
+
+    }
+}
